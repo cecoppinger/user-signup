2ac71f24970fedcf7b7a27ca791b624299b523e1
Clarence Coppinger <thea.trio@gmail.com>
2018-09-27 16:06:51 -0500

    Working of form verification

--- main.py
+++ main.py
@@ -7,4 +7,19 @@ app.config['DEBUG'] = True
 def index():
   return render_template("signup.html")
 
+@app.route('/welcome', methods=['POST'])
+def welcome():
+  username = request.form['username']
+  password = request.form['password']
+  verify_pw = request.form['verify-pw']
+  email = request.form['email']
+
+  if not username or not password or not verify_pw:
+    error_msg = "Please don't leave me empty!"
+    return redirect("/?error=" + error_msg)
+  
+
+  return render_template("welcome.html", name=username)
+
+
 app.run()
